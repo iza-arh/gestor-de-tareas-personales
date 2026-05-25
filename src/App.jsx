0c6b3ea5b922e7a522240381547c64b5934e648a
Iza <rh23003@ues.edu.sv.com>
2026-05-24 18:00:55 -0600

    feat:agregar sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
-    <>
-      <h1>hello world</h1>
-    </>
+      <div className="min-h-screen bg-background text-foreground">
+        {/* El sidebar */}
+        <Sidebar />
+
+        {/* Contenido principal */}
+        <main className="lg:pl-64 p-8 pt-20 lg:pt-8">
+        <h1 className="text-3xl font-bold text-center">Bienvenidos</h1>
+        </main>
+      </div>
   )
 }
 
