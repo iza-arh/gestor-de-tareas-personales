34ca248f0bb182362fb5d9a73cbf27010ac809a6
retro2047 <134110393+retro2047@users.noreply.github.com>
2026-05-31 17:29:18 -0600

    He creado categorias
    donde ingresa el nombre y la descripcion

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,42 @@
-import './App.css'
+import './App.css';
 import Sidebar from './components/Sidebar';
 import CrearMetaPage from './pages/metas/crearMetaPage';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ListaDeMetasPage from './pages/metas/ListaDeMetasPage';
+import CategoriasPage from './pages/categorias/CategoriaPage';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="min-h-screen bg-background text-foreground">
-        {/* El sidebar */}
-        <Sidebar />
+      <BrowserRouter>
+        <div className="min-h-screen bg-background text-foreground">
+          <Sidebar />
 
-        {/* Contenido principal */}
-        <main className="lg:pl-64 p-8 pt-20 lg:pt-8 flex flex-col items-center">
-          <h1 className="text-3xl font-bold text-center">Bienvenidos</h1>
-          <Routes>
-            {/* El path "/" suele ser la pantalla de inicio */}
-            <Route path="/metas-form-agregar-meta" element={<CrearMetaPage />} />
-            <Route path="/metas-list" element={<ListaDeMetasPage />} />
-          </Routes>
-        </main>
-      </div>
-    </BrowserRouter>
-  )
+          <main className="lg:pl-64 p-8 pt-20 lg:pt-8 flex flex-col items-center">
+            <h1 className="text-3xl font-bold text-center">
+              Bienvenidos
+            </h1>
+
+            <Routes>
+              <Route
+                path="/metas-form-agregar-meta"
+                element={<CrearMetaPage />}
+              />
+
+              <Route
+                path="/metas-list"
+                element={<ListaDeMetasPage />}
+              />
+
+              <Route
+                path="/categorias"
+                element={<CategoriasPage />}
+              />
+            </Routes>
+          </main>
+        </div>
+      </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
